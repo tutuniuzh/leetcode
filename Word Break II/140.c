b5ebaf079147e43d262466bf09d4a701c6fa7024
tutuniuzh <tutuniu@gmail.com>
2025-01-04 16:57:51 -0600

    add comments

--- Word Break II/140.c
+++ Word Break II/140.c
@@ -135,3 +135,36 @@ char** wordBreak(char* s, char** wordDict, int wordDictSize, int* returnSize) {
     return NULL;
 
 }
+
+
+/*
+    below is the backtrack way, much more concise same
+    exactly the same way, same time complexity
+    but my sloution use extra data structure which is a hash table
+    
+*/
+
+// #define MAXANS 100000
+// void backTrack(char *s, int i, char **dict, int dsz, int curp, char **ans, int *ansLen) {
+//     static char curr[MAXANS];
+//     int j, dLen;
+//     if (s[i] == '\0') {
+//         ans[*ansLen] = strdup(curr);
+//         *ansLen += 1;
+//         return;
+//     }
+//     if (curp > 0) curr[curp++] = ' ';
+//     for (j = 0; j < dsz; ++j) {
+//         dLen = strlen(dict[j]);
+//         if (strncmp(s+i, dict[j], dLen)) continue;
+//         strcpy(curr+curp, dict[j]);
+//         backTrack(s, i+dLen, dict, dsz, curp+dLen, ans, ansLen);
+//     }
+// }
+// char** wordBreak(char* s, char** wordDict, int wordDictSize, int* returnSize) {
+//     char **ans = (char**) malloc(MAXANS * sizeof(char*));
+//     int ansLen = 0;
+//     backTrack(s, 0, wordDict, wordDictSize, 0, ans, &ansLen);
+//     *returnSize = ansLen;
+//     return ans;
+// }
